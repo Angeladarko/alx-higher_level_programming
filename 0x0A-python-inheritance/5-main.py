b1e6af411125f2a228f84233e24788b97de242e8
Angeladarko <darkoakuffo177@gmail.com>
2023-09-11 07:33:53 -0700

    Python Inheritance

---
+++ 0x0A-python-inheritance/5-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
+
+bg = BaseGeometry()
+
+print(bg)
+print(dir(bg))
+print(dir(BaseGeometry))
+
+guillaume@ubuntu:~/0x0A$ ./5-main.py
